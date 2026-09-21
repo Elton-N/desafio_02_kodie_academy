e59b9064e9df0d9fce57fc053a6afa2fa8e1ca4d
Elton-N <eltonrj10@hotmail.com>
2026-09-21 18:02:03 -0300

    style: melhora hierarquia visual do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,18 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className="rodape">
-      <p>Dados fornecidos pela iTunes Search API.</p>
-     <p>Desenvolvido por <strong>Elton do Nascimento</strong> &bull; Projeto Educacional (Kodie Academy, 2026).</p>
-     <p><small>Este site não possui vínculo oficial com a banda Iron Maiden.</small></p>
+      <p className="rodape-marca">Iron Maiden Explorer</p>
+
+      <p className="rodape-credito">
+        Desenvolvido por <strong>Elton do Nascimento</strong>
+        <span className="rodape-separador">•</span>
+        Projeto Educacional (Kodie Academy, 2026)
+      </p>
+
+      <p className="rodape-info">Dados fornecidos pela iTunes Search API.</p>
+      <p className="rodape-aviso">
+        Este site não possui vínculo oficial com a banda Iron Maiden.
+      </p>
     </footer>
   )
 }
